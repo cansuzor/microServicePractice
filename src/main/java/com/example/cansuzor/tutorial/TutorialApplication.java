75fc5d37bd56ea91d13852ed15ea973c4364288e
Cansu Zor <cansuzor@Cansus-Air.lan>
2023-04-29 15:35:35 -0400

    added mongodb settings

--- src/main/java/com/example/cansuzor/tutorial/TutorialApplication.java
+++ src/main/java/com/example/cansuzor/tutorial/TutorialApplication.java
@@ -1,13 +1,16 @@
 package com.example.cansuzor.tutorial;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @ComponentScan("com.example.cansuzor.tutorial") //asking spring to detect spring components
 @EntityScan("com.example.cansuzor.tutorial.model")
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class TutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
--- src/main/java/com/example/cansuzor/tutorial/repo/CandidateRepo.java
+++ src/main/java/com/example/cansuzor/tutorial/repo/CandidateRepo.java
@@ -3,6 +3,7 @@ package com.example.cansuzor.tutorial.repo;
 import com.example.cansuzor.tutorial.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Repository
 public interface CandidateRepo extends JpaRepository<Candidate,Long> {
